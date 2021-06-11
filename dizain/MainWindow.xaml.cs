bab96c96edcf2f3179f95228909689033bf12e09
T1mba <timba0901989@gmail.com>
2021-06-12 00:25:00 +0300

    Соло

--- dizain/MainWindow.xaml.cs
+++ dizain/MainWindow.xaml.cs
@@ -25,47 +25,7 @@ namespace dizain
             InitializeComponent();
         }
 
-        private void Button_Reg_Click(object sender, RoutedEventArgs e)
-        {
-            string Login = textBoxLogin.Text.Trim();
-            string Pass = PassBox.Password.Trim();
-            string Pass2 = PassBox2.Password.Trim();
-            string Email = textBoxEmail.Text.ToLower().Trim();
-
-            if (Login.Length < 5)
-            {
-                textBoxLogin.ToolTip = "Логин должен состоять как минимум из пяти букв";
-                textBoxLogin.Background = Brushes.DarkRed;
-            }
-            else if (Pass.Length < 5)
-            {
-                PassBox.ToolTip = "Пароль должен состоять как минимум из пяти букв";
-                textBoxLogin.Background = Brushes.Red;
-            }
-            else if (Pass != Pass2)
-            {
-                PassBox2.ToolTip = "Пароли не совпадают";
-                textBoxLogin.Background = Brushes.Red;
-            }
-            else if (Email.Length < 5 || !Email.Contains("@") || !Email.Contains("."))
-            {
-                textBoxEmail.ToolTip = "Не верный формат почты";
-                textBoxLogin.Background = Brushes.Red;
-            }
-            else
-            {
-                textBoxLogin.ToolTip = " ";
-                textBoxLogin.Background = Brushes.Transparent;
-                PassBox.ToolTip = " ";
-                PassBox.Background = Brushes.Transparent;
-                PassBox2.ToolTip = " ";
-                PassBox2.Background = Brushes.Transparent;
-                textBoxEmail.ToolTip = " ";
-                textBoxEmail.Background = Brushes.Transparent;
-                MessageBox.Show("Регистрация прошла успешна");
-            }
-            
-        }
+        
         
     }
 }
