6e74a5afe8a02af78074294d79b91aa783b0a40f
T1mba <timba0901989@gmail.com>
2021-06-11 23:52:52 +0300

    Соло

---
+++ dizain/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dizain
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string Login = textBoxLogin.Text.Trim();
+            string Pass = PassBox.Password.Trim();
+            string Pass2 = PassBox2.Password.Trim();
+            string Email = textBoxEmail.Text.ToLower().Trim();
+
+            if (Login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Логин должен состоять как минимум из пяти букв";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (Pass.Length < 5)
+            {
+                PassBox.ToolTip = "Пароль должен состоять как минимум из пяти букв";
+                textBoxLogin.Background = Brushes.Red;
+            }
+            else if (Pass != Pass2)
+            {
+                PassBox2.ToolTip = "Пароли не совпадают";
+                textBoxLogin.Background = Brushes.Red;
+            }
+            else if (Email.Length < 5 || !Email.Contains("@") || !Email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Не верный формат почты";
+                textBoxLogin.Background = Brushes.Red;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = " ";
+                textBoxLogin.Background = Brushes.Transparent;
+                PassBox.ToolTip = " ";
+                PassBox.Background = Brushes.Transparent;
+                PassBox2.ToolTip = " ";
+                PassBox2.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = " ";
+                textBoxEmail.Background = Brushes.Transparent;
+                MessageBox.Show("Регистрация прошла успешна");
+            }
+            
+        }
+        
+    }
+}
